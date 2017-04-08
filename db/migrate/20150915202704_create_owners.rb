370cfb5268b960b2fded2e4f9e1e66a2b4444635
Elizabeth <elizabethnich19@gmail.com>
2017-04-08 11:42:15 -0400

    Done.

--- db/migrate/20150915202704_create_owners.rb
+++ db/migrate/20150915202704_create_owners.rb
@@ -1,11 +1,9 @@
 class CreateOwners < ActiveRecord::Migration
-  def up
-    create_table :owners do |t|
-      t.string :name
-    end
-  end
-  
-  def down
-    drop_table :owners
-  end
+    def change
+     create_table :owners do |t|
+       t.string :name
+       t.integer :age
+       t.string :breed
+     end
+   end
 end
